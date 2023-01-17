e6a73180b699728995720e6070a88b35fafc8c68
GhostDevMo <43588510+GhostDevMo@users.noreply.github.com>
2023-01-17 17:26:28 -0500

    Delete LNPopupController directory

--- LNPopupController/build/LNPopupController.build/Debug-iphoneos/LNPopupController.build/Objects-normal/arm64/LNPopupCustomBarViewController.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/Private/LNPopupCustomBarViewController.m \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/build/LNPopupController.build/Debug-iphoneos/LNPopupController.build/module.modulemap \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController.pch \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/Private/LNPopupCustomBarViewController+Private.h \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/LNPopupItem.h \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/LNPopupCustomBarViewController.h
--- LNPopupController/build/LNPopupController.build/Debug-iphoneos/LNPopupController.build/Objects-normal/arm64/NSObject+AltKVC.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/Private/NSObject+AltKVC.m \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/build/LNPopupController.build/Debug-iphoneos/LNPopupController.build/module.modulemap \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController.pch \
-  /Users/ojo/Downloads/DeepsoundiOSApp/LNPopupController/LNPopupController/Private/NSObject+AltKVC.h
